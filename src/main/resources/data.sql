ca7a16b6eb1a797cfc4e66d885df1d6f6deed348
Celso Furtado <celsofurtado@celsomacbook.local>
2025-10-22 15:44:48 -0300

    Adicionado mais uma receita

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -14,7 +14,8 @@
     INSERT INTO tbl_recipe (ID, TITLE, DESCRIPTION, DIFFICULT_LEVEL, COOKING_TIME, category_category_id, creation_date, url) VALUES (1, 'Torta de frango','Essa receita de torta de frango de liquidificador é deliciosa e muito prática para o dia a dia.','ADVANCED', 60, 1, '2025-10-05', '/images/torta_de_frango.jpg')
     INSERT INTO tbl_recipe (ID, TITLE, DESCRIPTION, DIFFICULT_LEVEL, COOKING_TIME, category_category_id, creation_date, url) VALUES (2, 'Bolo de cenoura','Confira essa clássica receita de bolo de cenoura simples e fácil, nossa campeã de acessos!','BEGINNER', 40, 7, '2025-09-05', '/images/bolo_de_cenoura.jpg')
     INSERT INTO tbl_recipe (ID, TITLE, DESCRIPTION, DIFFICULT_LEVEL, COOKING_TIME, category_category_id, creation_date, url) VALUES (3, 'Salada de berinjela','A salada de berinjela é uma opção saborosa e saudável para os amantes da culinária mediterrânea.','INTERMEDIATE', 30, 5, '2025-10-03', '/images/salada_de_berinjela.jpg')
-    ALTER TABLE tbl_recipe ALTER COLUMN ID RESTART WITH 4
+    INSERT INTO tbl_recipe (ID, TITLE, DESCRIPTION, DIFFICULT_LEVEL, COOKING_TIME, category_category_id, creation_date, url) VALUES (4, 'Strogonoff de frango','O strogonoff de frango é um prato amado por todos e muito fácil de ser preparado!','INTERMEDIATE', 45, 1, '2025-10-22', '/images/strogonoff_de_frango.jpg')
+    ALTER TABLE tbl_recipe ALTER COLUMN ID RESTART WITH 5
 
 -- CADASTRO DE INGREDIENTES
     INSERT INTO tbl_ingredient (ID, DESCRIPTION, RECIPE_ID) VALUES (1, '500 g de peito de frango sem pele', 1)
@@ -39,7 +40,14 @@
     INSERT INTO tbl_ingredient (ID, DESCRIPTION, RECIPE_ID) VALUES (18, '2 pimentões médios cortados em cubos', 3)
     INSERT INTO tbl_ingredient (ID, DESCRIPTION, RECIPE_ID) VALUES (19, '2 colheres de sopa de azeite', 3)
     INSERT INTO tbl_ingredient (ID, DESCRIPTION, RECIPE_ID) VALUES (20, '1 colher de sopa de shoyu ou molho inglês', 3)
-    ALTER TABLE tbl_ingredient ALTER COLUMN ID RESTART WITH 21
+
+    INSERT INTO tbl_ingredient (ID, DESCRIPTION, RECIPE_ID) VALUES (21, '1 kg de peito de frango em cubos', 4)
+    INSERT INTO tbl_ingredient (ID, DESCRIPTION, RECIPE_ID) VALUES (22, '2 cebolas picadinhas', 4)
+    INSERT INTO tbl_ingredient (ID, DESCRIPTION, RECIPE_ID) VALUES (23, '2 tomates picadinhos', 4)
+    INSERT INTO tbl_ingredient (ID, DESCRIPTION, RECIPE_ID) VALUES (24, '200 g de champignon', 4)
+    INSERT INTO tbl_ingredient (ID, DESCRIPTION, RECIPE_ID) VALUES (25, '1 caixinha de creme de leite', 4)
+    INSERT INTO tbl_ingredient (ID, DESCRIPTION, RECIPE_ID) VALUES (26, '2 colheres (sopa) de molho inglês', 4)
+    ALTER TABLE tbl_ingredient ALTER COLUMN ID RESTART WITH 27
 
 -- CADASTRO DE MÉTODOS DE PREPARO
     INSERT INTO tbl_preparation_method (ID, RECIPE_ID, DESCRIPTION) VALUES (1, 1, 'Cozinhe o peito de frango no caldo até ficar macio.')
@@ -61,4 +69,10 @@
     INSERT INTO tbl_preparation_method (ID, RECIPE_ID, DESCRIPTION) VALUES (15, 3, 'Jogue esses ingredientes já prontos na berinjela cozida e escorrida e misture tudo.')
     INSERT INTO tbl_preparation_method (ID, RECIPE_ID, DESCRIPTION) VALUES (16, 3, 'Esprema o limão, coloque o sal, a pimenta do reino, o orégano e o shoyo.')
     INSERT INTO tbl_preparation_method (ID, RECIPE_ID, DESCRIPTION) VALUES (17, 3, 'Leve à geladeira por no mínimo 4 horas e sirva com pães ou mesmo durante as refeições.')
-    ALTER TABLE tbl_preparation_method ALTER COLUMN ID RESTART WITH 18
+
+    INSERT INTO tbl_preparation_method (ID, RECIPE_ID, DESCRIPTION) VALUES (18, 4, 'Coloque o óleo em uma panela e frite o frango até quase dourar por completo.')
+    INSERT INTO tbl_preparation_method (ID, RECIPE_ID, DESCRIPTION) VALUES (19, 4, 'Acrescente o conhaque para flambar.')
+    INSERT INTO tbl_preparation_method (ID, RECIPE_ID, DESCRIPTION) VALUES (20, 4, 'Após as chamas apagarem, acrescente a cebola, o tomate e refogue.')
+    INSERT INTO tbl_preparation_method (ID, RECIPE_ID, DESCRIPTION) VALUES (21, 4, 'Acrescente o ketchup, a mostarda, o molho inglês e os champignons.')
+    INSERT INTO tbl_preparation_method (ID, RECIPE_ID, DESCRIPTION) VALUES (22, 4, 'Deixe cozinhar por 5 minutos, desligue o fogo e acrescente o creme de leite; mexa bem.')
+    ALTER TABLE tbl_preparation_method ALTER COLUMN ID RESTART WITH 23
